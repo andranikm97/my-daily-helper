4ac9b838b171e574e64315dccc79cf0156295cb5
Andranik Markaryan <andranik.mrk@gmail.com>
2021-10-28 02:29:25 +0300

    Feat: implement request logger
    Created a middleware function that logs request's method, path and
    ip.

--- server.js
+++ server.js
@@ -2,12 +2,22 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const connectToDB = require('./db/connect');
+const requestLogger = require('./middleware/requestLogger');
+const router = require('./routes/router');
 require('dotenv').config();
 
+// Chore
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+
+// Middleware
+
+// Routing
+app.use('/api/v1/tasks', router);
+
+// Not found
 
 // Start up
-
 const port = process.env.PORT;
 const mongoURI = process.env.MONGO_URI;
 

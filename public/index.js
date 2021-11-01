c105ff8b497f41a0b43baaa81458c573d168da1a
Andranik Markaryan <andranik.mrk@gmail.com>
2021-11-02 00:43:29 +0300

    Feat: implement custom not functionality
    Custom note functionality is now up and running. Either press '+', or 'Shift-Enter'

--- public/index.js
+++ public/index.js
@@ -31,26 +31,47 @@ const taskGenerator = (data) => {
   `;
 };
 
-$('.submission-form').on('submit', async (e) => {
+const submitTask = async (e) => {
   e.preventDefault();
 
   const url = 'http://localhost:5000/api/v1/tasks';
+  const inputBox = $('#input-box');
+
   try {
+    if (inputBox.val().trim() === '') {
+      throw new Error('Must enter text!', { cause: 'notext' });
+    }
+
     const response = await fetch(url, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify({
-        content: 'My first note',
+        content: inputBox.val(),
       }),
     });
 
     const data = await response.json();
-    console.log(data);
-
     $('.content').prepend(taskGenerator(data));
+
+    inputBox.val('');
   } catch (error) {
-    console.log(error);
+    switch (error.message) {
+      case 'Must enter text!':
+        // TODO: implement error appearing in UI, indicating that text must be entered
+        console.log("Empty strings won' be submitted to the DB.");
+        break;
+      default:
+        console.log('Some other error...');
+    }
+  }
+};
+
+document.addEventListener('keypress', (e) => {
+  if (e.code === 'Enter' && e.shiftKey) {
+    submitTask(e);
   }
 });
+
+$('.submission-form').on('submit', submitTask);
